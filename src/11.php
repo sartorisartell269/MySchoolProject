2392ffb0910110a3bcf7c8270c54ad1960a062de
sartorisartell269 <sartorisartell269@gmail.com>
2025-03-17 19:08:02 +0000

    Add 11.php

---
+++ src/11.php
@@ -0,0 +1,30 @@
+<?php
+
+// Database connection info
+$servername = "localhost";
+$username = "your_username";
+$password = "your_password";
+$dbname = "your_database";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// SQL query to retrieve data from database
+$sql = "SELECT * FROM your_table";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // Output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo $row["your_column"] . "<br>";
+    }
+} else {
+    echo "0 results";
+}
+
+?>
