7877befb4220bea88ea91f50f1890fc05bff38f5
sartorisartell269 <sartorisartell269@gmail.com>
2025-04-07 05:35:09 +0000

    Add 29.php

---
+++ src/29.php
@@ -0,0 +1,13 @@
+<?php
+// Include your database connection details
+$servername = "your_server_name";
+$username = "your_username";
+$password = "your_password";
+$dbname = "your_database_name";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
